25a8171bc0f8038d57fa21c9e57487d3d56f35cb
ZhangGuangPing <ping6270286@163.com>
2020-11-11 17:56:48 +0800

    切换位中文版本

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="app-container documentation-container">
-    <video src="@/assets/video/five-min.mp4" id='mmedia' width="640px" height="400px" style="background:black;"/>
-      
-    <input type="button" id="btn" @click="play" value="播放">
+    <video id="mmedia" width="80%" src="@/assets/video/five-min.mp4" />
+    <br>
+    <input id="btn" type="button" value="播放" @click="play">
   </div>
 </template>
 
 <script>
+
 export default {
-    name: "Documentation",
-    components: {},
-    data() {
-        return {
-          mmedia: '',
-        };
-    },
-    methods: {
-      play() {
-        if(mmedia.paused){// 播放
-              mmedia.play();
-              this.value = "暂停";
-          }else{// 暂停
-              mmedia.pause();
-              this.value = "播放";
-          }
-      },
-    },
-    created() {
-      this.mmedia = document.getElementById("mmedia");
-    },
-};
+  name: 'Documentation',
+  components: {},
+  data() {
+    return {
+    }
+  },
+  methods: {
+    play() {
+      const mmedia = document.getElementById('mmedia')
+      if (mmedia.paused) { // 播放
+        mmedia.play()
+        this.value = '暂停'
+      } else { // 暂停
+        mmedia.pause()
+        this.value = '播放'
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
