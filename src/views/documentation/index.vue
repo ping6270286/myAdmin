205ae2195fc1e43d310f0976d4d9c9b003977bd1
ZhangGuangPing <ping6270286@163.com>
2020-11-11 17:15:37 +0800

    视频上传

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="app-container documentation-container">
-    
+    <video src="@/assets/video/five-min.mp4" id='mmedia' width="640px" height="400px" style="background:black;"/>
+      
+    <input type="button" id="btn" @click="play" value="播放">
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Documentation',
-  components: {  },
-  data() {
-    return {
-    }
-  }
-}
+    name: "Documentation",
+    components: {},
+    data() {
+        return {
+          mmedia: '',
+        };
+    },
+    methods: {
+      play() {
+        if(mmedia.paused){// 播放
+              mmedia.play();
+              this.value = "暂停";
+          }else{// 暂停
+              mmedia.pause();
+              this.value = "播放";
+          }
+      },
+    },
+    created() {
+      this.mmedia = document.getElementById("mmedia");
+    },
+};
 </script>
 
 <style lang="scss" scoped>
